44685ef432fcba9d6b01779a17e1336a069661b5
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-09-12 09:10:20 +0700

    Initial commit

---
+++ lib/App/lcpan/Cmd/cpantesters_dist.pm
@@ -0,0 +1,60 @@
+package App::lcpan::Cmd::cpantesters_dist;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+use Perinci::Object;
+
+require App::lcpan;
+
+our %SPEC;
+
+$SPEC{handle_cmd} = {
+    v => 1.1,
+    summary => 'Open distribution page on CPAN Testers matrix',
+    description => <<'_',
+
+Given distribution `DIST`, this will open
+`https://matrix.cpantesters.org/?dist=`DIST`. `DIST` will first be checked for
+existence in local index database.
+
+_
+    args => {
+        %App::lcpan::common_args,
+        %App::lcpan::dists_args,
+    },
+};
+sub handle_cmd {
+    my %args = @_;
+
+    my $state = App::lcpan::_init(\%args, 'ro');
+    my $dbh = $state->{dbh};
+
+    my $envres = envresmulti();
+    for my $dist (@{ $args{dists} }) {
+        my ($file_id, $cpanid, $version) = $dbh->selectrow_array(
+            "SELECT file_id, cpanid, version FROM dist WHERE name=? AND is_latest", {}, $dist);
+        $file_id or do {
+            $envres->add_result(404, "No such dist '$dist'");
+            next;
+        };
+
+        require Browser::Open;
+        my $url = "https://matrix.cpantesters.org/?dist=$dist";
+        my $err = Browser::Open::open_browser($url);
+        if ($err) {
+            $envres->add_result(500, "Can't open browser for URL $url");
+        } else {
+            $envres->add_result(200, "OK");
+        }
+    }
+    $envres->as_struct;
+}
+
+1;
+# ABSTRACT:
---
+++ lib/App/lcpan/CmdBundle/cpantesters.pm
@@ -0,0 +1,19 @@
+package App::lcpan::CmdBundle::cpantesters;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: More lcpan subcommands related to CPAN Testers
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<lcpan>
+
+L<https://www.cpantesters.org>
